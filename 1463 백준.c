f66296f05580d0bac6e4870b2c523b940e2ff736
avcdevda <97731844+avcdevda@users.noreply.github.com>
2024-05-26 14:40:19 +0900

    다이나믹 프로그래밍 1463

---
+++ 1463 백준.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#define min(a,b) a<b?a:b
+
+
+int main(void)
+{
+
+    int dp[1000001]={0,0,1,1},n,cnt;
+    scanf("%d",&n);
+    for(int i=4;i<=n;i++)
+    {
+        if(i%6==0)
+        {
+            dp[i]=min(dp[i/3]+1,dp[i/2]+1);
+        }
+        else if(i%3==0)
+        {
+            dp[i]=min(dp[i/3]+1,dp[i-1]+1);
+        }
+        else if(i%2==0)
+        {
+            dp[i]=min(dp[i/2]+1,dp[i-1]+1);
+        }
+        else dp[i]=dp[i-1]+1;
+        
+
+    }
+    printf("%d",dp[n]);
+    return 0;
+}
